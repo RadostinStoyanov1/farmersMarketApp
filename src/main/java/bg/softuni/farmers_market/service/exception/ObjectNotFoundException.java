da3a84357973c71a39b6354050c7e074fda61563
RadostinStoyanov <Radostin_Stoyanov@students.softuni.bg>
2024-08-16 16:12:09 +0300

    Added global handler for not found object

---
+++ src/main/java/bg/softuni/farmers_market/service/exception/ObjectNotFoundException.java
@@ -0,0 +1,18 @@
+package bg.softuni.farmers_market.service.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class ObjectNotFoundException extends RuntimeException{
+    private final Object id;
+
+    public ObjectNotFoundException(String message, Object id) {
+        super(message);
+        this.id = id;
+    }
+
+    public Object getId() {
+        return id;
+    }
+}
